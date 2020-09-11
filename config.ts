78fc08a5f359bfa17396a77e83a5da37dfa6a0d5
takumi-yazawa-hs <43020978+takumi-yazawa-hs@users.noreply.github.com>
2020-09-11 18:10:22 +0900

    Initial commit

---
+++ config.ts
@@ -0,0 +1,12 @@
+const projectId = "aaa";
+export const config = {
+  // DO NOT USE THESE CREDENTIALS ! THEY ARE HERE TO HELP IN THE LEARNING PROCESS.
+  // ANY AND ALL DATA ON THAT DOMAIN IS SUBJECT TO CHANGE AND REMOVAL AT ANY TIME
+  // THIS ACCOUNT IS ALSO ON THE FREE PLAN AND IS SUBJECT TO RESTRICTIONS !
+  apiKey: "",
+  authDomain: projectId + ".firebaseapp.com",
+  databaseURL: "https://" + projectId + ".firebaseio.com",
+  projectId: projectId,
+  storageBucket: projectId + ".appspot.com",
+  messagingSenderId: ""
+};
